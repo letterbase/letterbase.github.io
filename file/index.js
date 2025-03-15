30bac24ec571e78c7e4ec43baf835dfc3e260e9a
letterbase <9946549@qq.com>
2025-03-15 21:53:59 +0800

    Create index.js

---
+++ file/index.js
@@ -0,0 +1,10 @@
+/*
+  INDEX.JS
+*/
+
+$(function(){
+  $('body').append('<div id="topleft">GITHUB源码</div>')
+  $('body').append('<div id="topbar"></div>')
+  $('body').append('<div id="sidebar"></div>');
+  $('body').append('<div id="content"></div>');
+})
