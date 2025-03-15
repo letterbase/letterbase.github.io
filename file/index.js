064fbfa3e7ee2a4c00a87f481697f6334be220ee
letterbase <9946549@qq.com>
2025-03-15 22:11:45 +0800

    Update index.js

--- file/index.js
+++ file/index.js
@@ -7,4 +7,11 @@ $(function(){
   $('body').append('<div id="topbar"></div>')
   $('body').append('<div id="sidebar"></div>');
   $('body').append('<div id="content"></div>');
+
+  if ($('html').attr('el-type') === 'status-404') {
+    $('#content').append('<h1 align="center">对不起</h1>')
+    $('#content').append('<h5 align="center">你的页面不存在！</h1>')
+    $('#content').append('<span align="center">如果你有<b>github</b>的话，请<a href="https://github.com/letterbase/letterbase.github.io/">编辑</a>页面</span>')
+    break;
+  }
 })
